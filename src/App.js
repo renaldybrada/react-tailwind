3f8389260ec1360060d8f3e3017d446884352c57
Muhammad Firdaus <mbex71@gmail.com>
2021-04-04 17:33:38 +0700

    change homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import OpenNew from 'assets/icons/open_new.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-800 min-h-screen">
+      <div className="flex justify-end px-8 h-12">
+        <a href="https://github.com/mbex71/react-tailwind" target="__blank" className="text-white items-center flex flex-row">
+          <img className="w-4 h-4 mr-2 text-blue-500" src={OpenNew} alt="https://github.com/mbex71/react-tailwind"/> Github</a>
+      </div>
+      <div className="flex min-h-screen justify-center items-center pb-12">
+        <h1 className="font-light text-xl md:text-6xl uppercase text-white font ">React Tailwind</h1>
+      </div>
+      
     </div>
   );
 }
